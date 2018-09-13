fdefabb14da9219117d8e2f1e7a33fa11de20add
Hitaro1985 <tomasfans1985@gmail.com>
2018-09-13 22:08:28 +0800

    api authenticate check enabled

--- app/Http/Controllers/API/ApiAuthUserCtrl.php
+++ app/Http/Controllers/API/ApiAuthUserCtrl.php
@@ -28,6 +28,12 @@ class ApiAuthUserCtrl extends Controller
     {
         // grab credentials from the request
         $credentials = $request->only('name', 'password');
+        $user = Admin::where(['name' => $request->name])->first();
+        if($user) {
+            if ($user->enabled == false) {
+                return response()->json(['message' => 'Your account has been blocked', 'data' => null, 'response_code' => 0], 500);
+            }
+        }
 
         try {
             // attempt to verify the credentials and create a token for the user
