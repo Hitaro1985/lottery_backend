c376eaec0aab60a6a16108bde4e7ef73776b1866
Hitaro1985 <tomasfans1985@gmail.com>
2018-10-14 13:42:49 +0800

    update Home screen api

--- app/Http/Controllers/API/ApiAgentController.php
+++ app/Http/Controllers/API/ApiAgentController.php
@@ -25,8 +25,8 @@ class ApiAgentController extends Controller
     {
             $cround = round::get()->first();
             $lround = roundlist::get()->last();
-            $passedrounds = roundlist::where('rightNumber', '!=', 'null')->orderBy('id', 'desc')->take(10)->get();
-            $totalResults = roundlist::where('rightNumber', '!=', 'null')->orderBy('id', 'desc')->take(3)->get();
+            $passedrounds = roundlist::whereNotNull('rightNumber')->orderBy('id', 'desc')->take(10)->get();
+            $totalResults = roundlist::whereNotNull('rightNumber')->orderBy('id', 'desc')->take(3)->get();
             $mjack = majorjackpot::get()->last();
             $jack = jackpot::get()->last();
             $red = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
