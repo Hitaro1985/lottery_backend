b7269b7547f1e26997d3f8af493c733d9ac18bc5
Hitaro1985 <tomasfans1985@gmail.com>
2018-10-15 00:46:06 +0800

    test getNowRound api

--- app/Http/Controllers/API/ApiAgentController.php
+++ app/Http/Controllers/API/ApiAgentController.php
@@ -608,7 +608,7 @@ class ApiAgentController extends Controller
             $round = round::get()->first();
             return response()->json(['message' => "Get Now Round", 'data' => ['round'=> $round["roundname"]], 'response_code' =>1], 200);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Request Error', 'data' => null, 'response_code' => 0], 200);
+            return response()->json(['message' => 'Request Error', 'data' => $e->getMessage(), 'response_code' => 0], 200);
         }
     }
 
